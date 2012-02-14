26a2f7c4c4d40e6dbef0e0b5e11b5e5ffde72dd9
iain <iain@iain.nl>
2012-02-14 21:12:01 +0100

    Silence Churn output

--- lib/metrical/metrics/churn.rb
+++ lib/metrical/metrics/churn.rb
@@ -4,10 +4,18 @@ module Metrical
   module Metrics
     class Churn
 
+      class Calculator < ::Churn::ChurnCalculator
+
+        # Silenced output
+        def self.git?
+          system "git branch > /dev/null 2>&1"
+        end
+      end
+
       def self.run(options = nil)
         options ||= { :minimum_churn_count => 0 }
         output_string = ""
-        ::Churn::ChurnCalculator.new(options).report(false)[:churn]
+        Calculator.new(options).report(false)[:churn]
       end
 
     end
